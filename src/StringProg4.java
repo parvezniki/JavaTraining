e2703bf0a0696337155dada39d76bbeed41ac345
Parvez Iqbal <parvez.ira@gmail.com>
2023-11-25 17:11:41 +0530

    25th Nov - Programs

--- src/StringProg4.java
+++ src/StringProg4.java
@@ -1,15 +1,14 @@
 public class StringProg4 {
     public static void main(String[] args) {
-        /* String s1 = "apple";
+         String s1 = "apple";
         String s2 = "apple";
         System.out.println(s1.equals(s2));
         System.out.println(s1.equalsIgnoreCase(s2));
         // Changing of Case
         System.out.println("Convert to Upper Case: "+s1.toUpperCase());
-        */
+
 
         System.out.println("This is how you print a \\ ");    //   \    --> \\
-
         System.out.println(" you can write \" anything here ");  //  \ -> Escape sequence char    \"
         System.out.println(" you can write \' anything here ");  //  \ -> Escape sequence char    \'
 
